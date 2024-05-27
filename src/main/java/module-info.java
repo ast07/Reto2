7cb4de6bcb27f579b089b23b445e2dade527f27f
adri1 <adridg01@gmail.com>
2024-05-27 22:15:45 +0200

    commit "final"

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.retoajedrez {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.example.retoajedrez to javafx.fxml;
-    exports com.example.retoajedrez;
-}
